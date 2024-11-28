43822e76bae05601692de8e7fb9f92c7bcf9ad2a
taeeonkim <myhom43@gmail.com>
2024-11-28 12:42:32 +0900

    프로젝트 생성

---
+++ src/main/java/stock/mock_stock/MockStockApplication.java
@@ -0,0 +1,13 @@
+package stock.mock_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stock/mock_stock/MockStockApplicationTests.java
@@ -0,0 +1,13 @@
+package stock.mock_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
