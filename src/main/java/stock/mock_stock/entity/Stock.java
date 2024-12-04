d0ce4804165941c073ff7f036d49d54ca3398317
taeeonkim <myhom43@gmail.com>
2024-12-05 05:57:09 +0900

    entity nullable false 설정, StockSearchRepository로 이름변경 및 Jpa query 작성

--- src/main/java/stock/mock_stock/entity/Stock.java
+++ src/main/java/stock/mock_stock/entity/Stock.java
@@ -12,19 +12,26 @@ public class Stock {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String stckName;
+    @Column(nullable = false)
     private String stckNameEng;
+    @Column(nullable = false)
     private String stckNameAbr;
 
-    @Column(length = 12)
+    @Column(length = 12, nullable = false)
     private String stckCode;
-    @Column(length = 12)
+    @Column(length = 12, nullable = false)
     private String stckCodeStand;
+    @Column(nullable = false)
     private String marketType;
-    @Column(length = 12)
+    @Column(length = 12, nullable = false)
     private String securityType;
+    @Column(nullable = false)
     private LocalDate listingDate;
+    @Column(nullable = false)
     private Long listedShareQty;
+    @Column(nullable = false)
     private Long faceValue;
 
 }
