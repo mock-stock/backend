e7ce8d6882308b2ae4fe4f10928960b93cf2689d
taeeonkim <myhom43@gmail.com>
2024-12-16 09:56:44 +0900

    Stock 공통 레퍼지터리로 이름변경, 주식 단일 종목 쿼리추가

--- src/main/java/stock/mock_stock/service/StockSearchServiceImpl.java
+++ src/main/java/stock/mock_stock/service/StockSearchServiceImpl.java
@@ -2,10 +2,9 @@ package stock.mock_stock.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
 import stock.mock_stock.dto.StockSearchResultDto;
 import stock.mock_stock.entity.Stock;
-import stock.mock_stock.repository.StockSearchRepository;
+import stock.mock_stock.repository.StockRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,13 +13,12 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class StockSearchServiceImpl implements StockSearchService{
 
-    private final StockSearchRepository stockSearchRepository;
-
+    private final StockRepository stockRepository;
 
     @Override
     public List<StockSearchResultDto> searchStocks(String searchQuery) {
         // 레포지토리에서 검색 수행
-        List<Stock> stocks = stockSearchRepository.findAllByQuery(searchQuery);
+        List<Stock> stocks = stockRepository.findAllByQuery(searchQuery);
         System.out.println("stocks = " + stocks);
         // 검색 결과를 DTO로 변환
         return stocks.stream().map(stock -> {
