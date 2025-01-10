57b4a552a2239943f4629ebb1d4cc5bf7195d920
taeeonkim <myhom43@gmail.com>
2025-01-10 22:09:18 +0900

    기본 시간 seoul asia로 설정

--- src/main/java/stock/mock_stock/MockStockApplication.java
+++ src/main/java/stock/mock_stock/MockStockApplication.java
@@ -3,10 +3,14 @@ package stock.mock_stock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MockStockApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("Default TimeZone set to Asia/Seoul");
 		SpringApplication.run(MockStockApplication.class, args);
 	}
 
