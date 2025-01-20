2b01f444669ca8a887fc3ab8abcb7545e7f8c0b2
taeeonkim <myhom43@gmail.com>
2025-01-20 15:07:14 +0900

    일별분봉조회 추가, 공통 Dto 필드 추가, TODO 작성

--- src/main/java/stock/mock_stock/service/KISApiService.java
+++ src/main/java/stock/mock_stock/service/KISApiService.java
@@ -2,13 +2,15 @@ package stock.mock_stock.service;
 
 import stock.mock_stock.dto.OAuthToken;
 import stock.mock_stock.dto.StockInfoOutput;
+import stock.mock_stock.dto.StockKisHistoryDto;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface KISApiService {
 
     public StockInfoOutput getDomesticStockInfo(String fidCondMrktDivCode, String fidInputIscd);
-    public StockInfoOutput getStockHistoryInfo(String fidCondMrktDivCode, String fidInputIscd, LocalDate fidInputDate1, LocalDate fidInputDate2, String interval);
+    public List<StockKisHistoryDto> getStockHistoryInfo(String fidCondMrktDivCode, String fidInputIscd, LocalDate fidInputDate1, LocalDate fidInputDate2, String interval);
     public OAuthToken fetchOauthToken(String grantType, String appKey, String appSecret);
     public OAuthToken getTokenInfo(String key);
     public void saveTokenInfo(String key, OAuthToken oAuthToken);
