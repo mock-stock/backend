49228527c35b55dd384ce6abff5b4a55f58f51c7
taeeonkim <myhom43@gmail.com>
2025-01-27 16:24:36 +0900

    type 을위해 Notnull 필드 어노테이션선언

--- src/main/java/stock/mock_stock/dto/StockKisHistoryDto.java
+++ src/main/java/stock/mock_stock/dto/StockKisHistoryDto.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -13,12 +13,14 @@ import lombok.Data;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class StockKisHistoryDto {
 
+    @NotNull
     private String stckName;
 
     @JsonProperty("array_index")
     @Schema(nullable = true)
     private Integer arrayIndex;
 
+    @NotNull
     @JsonProperty("stck_bsop_date")
     private String stckBsopDate;
 
@@ -30,18 +32,23 @@ public class StockKisHistoryDto {
     @Schema(nullable = true)
     private Long stckPrpr;
 
+    @NotNull
     @JsonProperty("stck_oprc")
     private Long stckOprc;
 
+    @NotNull
     @JsonProperty("stck_clpr")
     private Long stckClpr;
 
+    @NotNull
     @JsonProperty("stck_hgpr")
     private Long stckHgpr;
 
+    @NotNull
     @JsonProperty("stck_lwpr")
     private Long stckLwpr;
 
+    @NotNull
     @JsonProperty("acml_vol")
     private Long acmlVol;
 
@@ -50,10 +57,11 @@ public class StockKisHistoryDto {
     @Schema(nullable = true)
     private Long cntgVol;
 
-
+    @NotNull
     @JsonProperty("prdy_vrss")
     private Long prdyVrss;
 
+    @NotNull
     @JsonProperty("stck_change_rate")
     private Double stckChangeRate;
 
--- src/main/java/stock/mock_stock/dto/StockSearchResultDto.java
+++ src/main/java/stock/mock_stock/dto/StockSearchResultDto.java
@@ -2,13 +2,17 @@ package stock.mock_stock.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class StockSearchResultDto {
+    @NotNull
     private Long sid;
+    @NotNull
     private String stckName;
+    @NotNull
     private String stckCode;
 
 
