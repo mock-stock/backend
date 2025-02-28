f5f63996672a4b67160560737bf2963e0e58a624
taeeonkim <myhom43@gmail.com>
2025-02-28 16:28:04 +0900

    feature: 관심종목 get controlller  추가

---
+++ src/main/java/stock/mock_stock/controller/WatchlistController.java
@@ -0,0 +1,30 @@
+package stock.mock_stock.controller;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import stock.mock_stock.dto.WatchlistResponseDto;
+import stock.mock_stock.service.WatchlistService;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/stocks")
+@RequiredArgsConstructor
+public class WatchlistController {
+    private final WatchlistService watchlistService;
+    @GetMapping("/watchlist")
+    public List<WatchlistResponseDto> getWatchlist(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated() && authentication.getPrincipal() instanceof Claims claims) {
+            Long userId = Long.valueOf(claims.getSubject());
+            return watchlistService.getWatchList(userId);
+        }
+        return Collections.emptyList();
+        }
+}
