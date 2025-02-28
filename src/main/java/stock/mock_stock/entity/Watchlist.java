a714322a320f7a48b6cb3e4d8c851f5fd1bc2e71
taeeonkim <myhom43@gmail.com>
2025-02-28 14:13:19 +0900

    관심종목 watchlist entity 추가

---
+++ src/main/java/stock/mock_stock/entity/Watchlist.java
@@ -0,0 +1,16 @@
+package stock.mock_stock.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Watchlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long wid;
+
+    @Column(name = "uid", nullable = false)
+    private Long uid;  // User를 참조하지 않고 uid 값만 저장
+
+    @Column(name = "sid", nullable = false)
+    private Long sid;  // Stock을 참조하지 않고 sid 값만 저장
+}
