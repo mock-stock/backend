b932166791ccd82a736a905bc3b41afd2487e5fa
taeeonkim <myhom43@gmail.com>
2025-03-02 15:36:09 +0900

    entity user, account, socialAccount 영속성 전이 수정

--- src/main/java/stock/mock_stock/entity/Account.java
+++ src/main/java/stock/mock_stock/entity/Account.java
@@ -13,12 +13,12 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long acid;
 
-    @Column(name = "uid", nullable = false, insertable = false, updatable = false) // 외래키 직접 관리
-    private Long uid;
-
-    private Long balance;
-
     @OneToOne
     @JoinColumn(name = "uid")  // User를 직접 참조하지 않고 uid로 관리
     private User user;
+
+    @Column(columnDefinition = "BIGINT DEFAULT 0")  // DB에서 기본값 0 설정
+    private Long balance;
+
+
 }
