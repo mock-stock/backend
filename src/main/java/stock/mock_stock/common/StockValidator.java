3e4bd12b2f3675388ad3173944e3234a0197b6cf
taeeonkim <myhom43@gmail.com>
2025-03-15 16:29:40 +0900

    EntityNotFoundException으로 변경

--- src/main/java/stock/mock_stock/common/StockValidator.java
+++ src/main/java/stock/mock_stock/common/StockValidator.java
@@ -1,12 +1,13 @@
 package stock.mock_stock.common;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Component;
 import stock.mock_stock.entity.Stock;
 import stock.mock_stock.exception.InvalidStockCodeException;
 
-import stock.mock_stock.exception.NotFoundException;
+
 import stock.mock_stock.repository.StockRepository;
 
 @Component
@@ -18,7 +19,7 @@ public class StockValidator {
     public Stock getStock(String stckCode) {
         if(stckCode.length() != 6) throw new InvalidStockCodeException("Invalid stock code : " + stckCode);
         Stock stock = stockRepository.findByStockCode(stckCode);
-        if(stock == null) throw new NotFoundException("Not Found stock code: " + stckCode);
+        if(stock == null) throw new EntityNotFoundException("Not Found stock code: " + stckCode);
         return stock;
     }
 }
--- src/main/java/stock/mock_stock/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package stock.mock_stock.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
